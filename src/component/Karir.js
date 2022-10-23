3ebcd9f69042eacccc3e195002f844e91acf171b
AgiSutrisna <Agywae45@gmail.com>
2022-10-23 16:21:38 +0700

    validasi form

--- src/component/Karir.js
+++ src/component/Karir.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 import Cover from "../assets/img/cover.png";
 
+
+
 function Karir() {
+
+  const initialValues = { nama: "", email: "", tlpn: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.nama) {
+      errors.nama = "Nama is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.tlpn) {
+      errors.tlpn = "No handphone is required";
+    } 
+    return errors;
+  };
+
   return (
     <div className='flex justify-center px-5'>
         <div className='flex flex-col'>
@@ -11,7 +53,7 @@ function Karir() {
             <div className='flex flex-col justify-center items-center py-20 gap-11'>
                 <h1 className='text-2xl lg:text-5xl font-bold text-[#484949]'>Mulai Karir di Donatsu</h1>
                 <p className='text-xl lg:text-2xl font-normal text-[#484949] w-[330px] lg:w-[440px] h-[72px] text-center'>Gabung kemitraan dan dapat keuntung menarik</p>
-                <form className="gap-11 pt-3 justify-center items-center">
+                <form onSubmit={handleSubmit} className="gap-11 pt-3 justify-center items-center">
                     <label className="block gap-4">
                         <input
                             className="border w-[330px] lg:w-[642px] h-14 rounded border-[#DADADA] pl-3 font-sans"
@@ -19,7 +61,10 @@ function Karir() {
                             type="text"
                             name="nama"
                             id='nama'
+                            value={formValues.nama}
+                            onChange={handleChange}
                         />
+                        <p className="text-red-600">{formErrors.nama}</p>
                     </label>
                     <label className="block pt-11">
                         <input
@@ -28,16 +73,22 @@ function Karir() {
                         type="text"
                         name="email"
                         id='email'
+                        value={formValues.email}
+                        onChange={handleChange}
                         />
+                        <p className="text-red-600">{formErrors.email}</p>
                     </label>
                     <label className="block pt-11">
                         <input
                         className="border w-full lg:w-[642px] h-14 rounded border-[#DADADA] pl-3 font-sans"
                         placeholder="No Handphone"
-                        type="text"
+                        type="number"
                         name="tlpn"
                         id='tlpn'
+                        value={formValues.tlpn}
+                        onChange={handleChange}
                         />
+                        <p className="text-red-600">{formErrors.tlpn}</p>
                     </label>
                     <label className="block pt-11">
                         <select class="border w-full lg:w-[642px] h-14 rounded text-[#616364] border-[#DADADA] pl-3 font-sans  bg-white" id="grid-state">
@@ -56,10 +107,13 @@ function Karir() {
                         id='resume'
                         />
                     </label>
+                
+                    <div className="flex justify-center">
+                        <button className='flex flex-row justify-center items-center gap-2 bg-[#1FB2FC] border rounded-2xl w-[300px] lg:w-[532px] h-[52px] mt-5'>
+                            <p className='text-sm lg:text-lg font-normal text-[#FFFFFF]'>DAPATKAN PENAWARAN KERJASAMA</p>  
+                        </button>
+                    </div>
                 </form>
-                <button className='flex flex-row justify-center items-center gap-2 bg-[#1FB2FC] border rounded-2xl w-[300px] lg:w-[532px] h-[52px]'>
-                    <p className='text-sm lg:text-lg font-normal text-[#FFFFFF]'>DAPATKAN PENAWARAN KERJASAMA</p>  
-                </button>
             </div>
         </div>
     </div>
--- src/component/LandingPage.js
+++ src/component/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 import Gambar1 from "../assets/img/Ilustrasi.png";
 import Gambar2 from "../assets/img/keluarga.png";
 import Gambar3 from "../assets/img/sisteur.png";
@@ -19,6 +19,44 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function LandingPage() {
+
+  const initialValues = { nama: "", email: ""};
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.nama) {
+      errors.nama = "Nama is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    
+    return errors;
+  };
+
   var settings = {
     dots: true,
     infinite: true,
@@ -162,7 +200,7 @@ function LandingPage() {
     <div className='flex flex-col justify-center items-center py-20 gap-11'>
         <h1 className='text-2xl lg:text-5xl font-normal text-[#484949]'>Gabung Kemitraan Donatsu</h1>
         <p className='text-xl lg:text-2xl font-normal text-[#484949] w-auto lg:w-[440px] h-[72px] text-center'>Gabung kemitraan dan dapat keuntung menarik</p>
-        <form className="gap-11 pt-3 justify-center items-center">
+        <form  onSubmit={handleSubmit} className="gap-11 pt-3 justify-center items-center">
           <label className="block gap-4">
             <input
               className="border w-[330px] lg:w-[642px] h-14 rounded border-[#DADADA] pl-3 font-sans"
@@ -170,7 +208,10 @@ function LandingPage() {
               type="text"
               name="nama"
               id='nama'
+              value={formValues.nama}
+              onChange={handleChange}
             />
+            <p className="text-red-600">{formErrors.nama}</p>
           </label>
           <label className="block pt-11">
             <input
@@ -179,13 +220,18 @@ function LandingPage() {
               type="text"
               name="email"
               id='email'
+              value={formValues.email}
+              onChange={handleChange}
             />
+            <p className="text-red-600">{formErrors.email}</p>
           </label>
-          </form>
-          <button className='flex flex-row justify-center items-center gap-2 bg-[#1FB2FC] border rounded-2xl w-[300px] lg:w-[532px] h-[52px]'>
-              <p className='text-sm lg:text-lg font-normal text-[#FFFFFF]'>DAPATKAN PENAWARAN KERJASAMA</p>  
-          </button>
-    </div>
+          <div className="flex justify-center">
+              <button className='flex flex-row justify-center items-center gap-2 bg-[#1FB2FC] border rounded-2xl w-[300px] lg:w-[532px] h-[52px] mt-5'>
+                <p className='text-sm lg:text-lg font-normal text-[#FFFFFF]'>DAPATKAN PENAWARAN KERJASAMA</p>  
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
